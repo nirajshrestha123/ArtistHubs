a0944ed966e3fdc8fec7359b7651be8e50941473
niraj shrestha sherpa <nirajshresthasherpa@gmail.com>
2020-02-16 22:17:33 +0545

    Post and Mongodb module

--- models/post.js
+++ models/post.js
@@ -1,18 +1,18 @@
-let mongoose = require('mongoose');
+// let mongoose = require('mongoose');
 
-let posts = mongoose.Schema({
-    title:{
-        type: String,
-        required: true
-    },
-    file:{
-        type: String,
-        required: true
-    },
-    content:{
-        type: String,
-        required: true
-    },
-});
+// let posts = mongoose.Schema({
+//     title:{
+//         type: String,
+//         required: true
+//     },
+//     file:{
+//         type: String,
+//         required: true
+//     },
+//     content:{
+//         type: String,
+//         required: true
+//     },
+// });
 
-let Post = module.exports = mongoose.model('Post', posts);
+// let Post = module.exports = mongoose.model('Post', posts);

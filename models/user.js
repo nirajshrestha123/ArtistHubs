ea981f3a861287f1e99eb4fc3435ea4b0e490cd1
niraj shrestha sherpa <nirajshresthasherpa@gmail.com>
2020-02-16 22:17:56 +0545

    user and Mongodb module

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
-let mongoose = require('mongoose');
+// let mongoose = require('mongoose');
 
-let user = mongoose.Schema({
-    email:{
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-});
+// let user = mongoose.Schema({
+//     email:{
+//         type: String,
+//         required: true
+//     },
+//     password:{
+//         type: String,
+//         required: true
+//     },
+// });
 
-let User = module.exports = mongoose.model('User', user);
+// let User = module.exports = mongoose.model('User', user);
